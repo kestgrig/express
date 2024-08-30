407599bfecfd1402531e0a0d2ad7a43ee20b2330
kestgrig <kestutis.grigenas@gmail.com>
2024-08-30 18:37:45 +0300

    express

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -2,22 +2,24 @@ import express from 'express';
 
 export const apiRouter = express.Router();
 
+
+//GET: /api/
 apiRouter.get('/', (req, res) => {
-    const data = {
+    const data = {     //duomenys
         state: 'error',
         message: 'Nurodyk konkretu API endpoint\'a',
     };
-    return res.json(data);
+    return res.json(data); //json formatu
 });
 
-const marks = [];
+const marks = [];  //tuscias masyvas iskeltas pries 
 
-apiRouter.get('/my-marks', (req, res) => {
+apiRouter.get('/my-marks', (req, res) => { //gauti
     return res.json(marks);
 });
 
-apiRouter.post('/my-marks', (req, res) => {
-    marks.push(req.body.mark);
+apiRouter.post('/my-marks', (req, res) => { //sukurti
+    marks.push(req.body.mark); //pridedami pazymiai
 
     return res.json({
         state: 'success',
@@ -25,10 +27,10 @@ apiRouter.post('/my-marks', (req, res) => {
     });
 });
 
-apiRouter.put('/my-marks/:index', (req, res) => {
-    const { index } = req.params;
+apiRouter.put('/my-marks/:index', (req, res) => { //papildyti 
+    const { index } = req.params; //pozicija indeksas
     const position = parseFloat(index);
-    const newMarkValue = req.body.newMark;
+    const newMarkValue = req.body.newMark; //naujas pazimys
 
     if (!Number.isInteger(position) || position < 0) {
         return res.json({
@@ -61,32 +63,34 @@ apiRouter.put('/my-marks/:index', (req, res) => {
     });
 });
 
-apiRouter.delete('/my-marks/:index', (req, res) => {
-    const { index } = req.params;
-    const position = parseFloat(index);
+apiRouter.delete('/my-marks/:index', (req, res) => { //istrinami 
+    const { index } = req.params; //issitarukiu index ir params
+    const position = parseFloat(index); //pozicija issiparsinama is index
 
-    if (!Number.isInteger(position) || position < 0) {
+    //apsaugos
+
+    if (!Number.isInteger(position) || position < 0) { // jeigu nera skaicius integer arba pozicija maziau uz 0
         return res.json({
             state: 'error',
             message: 'Pazymio pozicija (index) turi buti ne neigiamas sveikasis skaicius',
         });
     }
 
-    if (marks.length === 0) {
+    if (marks.length === 0) { //jeigu ilgis yra 0
         return res.json({
             state: 'error',
             message: 'Pazymiu sarasas ir taip jau yra tuscias... nera ko papildomai salinti',
         });
     }
 
-    if (position >= marks.length) {
+    if (position >= marks.length) { //jeigu pozicija didesne arba lygi pazimiu ilgiui
         return res.json({
             state: 'error',
             message: `Norimo pasalinti pazymio indexas negali virsyti leistinos ribos (riba: ${marks.length - 1}).`,
         });
     }
 
-    marks.splice(position, 1);
+    marks.splice(position, 1); //pazymiai funkcija splice pasalina po 1
 
     return res.json({
         state: 'success',
